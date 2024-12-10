a4669a2bf2fb63fa0b698e417458f6a346fac4fe
Tu08112003 <huynhtu012023@gmail.com>
2024-12-10 11:57:36 +0700

    done todo-list

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,93 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+  export default {
+  data() {
+    return {
+      newTask: '',
+      tasks: []
+    };
+  },
+  methods: {
+    addTask() {
+      if (this.newTask.trim()) {
+        this.tasks.push({ text: this.newTask, completed: false });
+        this.newTask = '';
+      }
+    },
+    deleteTask(index) {
+      this.tasks.splice(index, 1);
+    }
+  }
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="container">
+      <div class="header">
+        <h1>My Todo-S</h1>
+        <div class="input-todo">
+          <input type="text" 
+            v-model="newTask"
+            placeholder="Add new..."
+            v-on:keyup.enter="addTask"
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+          />
+          <button v-on:click="addTask">Add</button>
+        </div>
+      </div>
+      <div class="todo-list">
+        <ul>
+          <li v-for="(task, index) in tasks" :key="index">
+          <input type="checkbox" 
+             v-model="task.completed" 
+          />
+          <span :class="{ completed: task.completed }">{{ task.text }}</span>
+          <button v-on:click="deleteTask(index)">Delete</button>
+        </li>
+        </ul>
+      </div>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  .container{
+    display: grid;
+    grid-template-columns: 1fr, 1fr;
+    gap: 2rem;
+    width: 90%;
+  }
+  .header{
+    padding: 2rem;
+    align-items: center;
+    text-align: center;
+  }
+  .input-todo{
+    display: grid;
+    grid-template-columns: 4fr 0.5fr; 
+    align-items: center;
+    margin-top: 2rem;
+  }
+  .input-todo input{
+    height: 2rem;
+    border-radius: 5px;
+    margin-right: 1rem;
+    padding: 1rem 1rem;
+  }
+  button{
+    width: 4rem;
+    height: 2rem;
+    border-radius: 5px;
+  }
+  .todo-list li{
+    display: grid;
+    grid-template-columns: 1fr 3fr 1fr; 
+    align-items: center;
+    gap: 1rem; 
+    padding: 0.5rem;
+    border-bottom: 1px solid #ddd; 
+    list-style: none;
+  }
+  .todo-list li .completed {
+    text-decoration: line-through;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
